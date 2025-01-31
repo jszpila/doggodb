3790dcdd971379d91caa7f15bc85bf8a8207b7dd
Jan <153112073+jan-unib@users.noreply.github.com>
2025-01-31 09:08:31 -0600

    Adds router, icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import './themes/index.scss'
-import './App.scss';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { AppContext } from "./AppContext";
+import { Themes } from "./components/enum/themes";
 
 import Header from './components/Header/Header';
+import LoginForm from "./components/LoginForm/LoginForm";
+
+import "./themes/index.scss";
+import "./App.scss";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="content-container">
-        <Header />
-        <main className="main-content">
+  const [user, setUser] = useState<string>('');
+  const [theme, setTheme] = useState<string>(Themes.Light);
 
-        </main>
-      </div>
-    </div>
+  const appCtx = {
+    user,
+    setUser,
+    theme,
+    setTheme,
+  };
+
+  useEffect(() => {
+    const docBody = document.body;
+    docBody.classList.toggle(Themes.Light, theme === Themes.Light);
+    docBody.classList.toggle(Themes.Dark, theme === Themes.Dark);
+  }, [theme]);
+
+  return (
+    <AppContext.Provider value={appCtx}>
+      <AppContext.Consumer>
+        {(value) => (
+          <div className="App">
+            <div className="content-container">
+              <Header />
+              <main className="main-content">
+                <Router>
+                  <Routes>
+                    <Route path="/" element={ <LoginForm /> } />
+                  </Routes>
+                </Router>
+              </main>
+            </div>
+          </div>
+        )}
+      </AppContext.Consumer>
+    </AppContext.Provider>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,36 @@
 
 
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../../AppContext';
+import { Themes } from '../enum/themes';
+import { FiSun, FiMoon } from 'react-icons/fi';
+
 import './Header.scss';
 
 export default function Header() {
+  const [icon, setIcon] = useState(<FiMoon />);
+  const context = useContext(AppContext);
+
+  function handleThemeChange(): void { 
+    setIcon(context.theme === Themes.Light ? <FiSun /> : <FiMoon />)
+    context.setTheme(context.theme === Themes.Light ? Themes.Dark : Themes.Light);
+  }
+
   return (
     <header className="header">
       <div className="header__title-container">
         <h1 className="header__title">DoggoDB™</h1>
         <h2 className="header__subtitle">Find Your Furry Friend</h2>
       </div>
-      <div className="header__nav-container">BUTTONS!</div>
+      <div className="header__nav-container">
+        <button
+          type="button"
+          className="btn--tertiary"
+          onClick={handleThemeChange}
+        >
+          {icon}
+        </button>
+      </div>
     </header>
   );
 }
