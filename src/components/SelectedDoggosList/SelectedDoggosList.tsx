0633165f16dc2d37cc7877395eef8e6a4b9fe5fa
Jan <153112073+jan-unib@users.noreply.github.com>
2025-02-05 22:44:17 -0600

    Remove unnecessary files

--- src/components/SelectedDoggosList/SelectedDoggosList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./SelectedDoggosList.scss";
-
-export default function SelectedDoggosList() {
-  return <section className="selected-doggos-list"></section>;
-}
